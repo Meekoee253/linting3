78b08bcdc4b84b8b65877376a75110695d6a2fc3
Meekoee253 <114604279+Meekoee253@users.noreply.github.com>
2023-03-16 15:49:45 +0000

    final

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+//heres our data for users
+var data = require('./data/test.json');
+
+app.get('/', (req, res) => {
+  var title = 'Home Page';
+  res.render('pages/index', { 'title': title });
+})
+
+app.get('/about', (req, res) => {
+  var title = 'About Page';
+  res.render('pages/about', { 'title': title });
+})
+
+app.get('/food', (req, res) => {
+  var title = 'food Page';
+  res.render('pages/food', { 'title': title });
+})
+
+app.get('/sports', (req, res) => {
+  var title = 'sports Page';
+  res.render('pages/sports', { 'title': title });
+})
+
+app.get('/music', (req, res) => {
+  var title = 'music Page';
+  res.render('pages/music', { 'title': title });
+})
+
+app.get('/games', (req, res) => {
+  var title = 'games Page';
+  res.render('pages/games', { 'title': title });
+})
+
+//users index is our list page
+app.get('/users', function(req, res) {
+  var title = 'Users Page';
+  res.render('users/index', {
+    title: title,
+    users: data
+  });
+
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+  var title = 'User Page';
+  var id = req.params.id;
+  res.render('users/view', {
+    title: title,
+    user: data[--id]
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  console.log(data);
+})
